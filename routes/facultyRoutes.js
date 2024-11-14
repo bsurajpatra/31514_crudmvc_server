d710b18b1838af342b9849017573c77f571cddc6
bsurajpatra <ankitsuraj1111@gmail.com>
2024-11-14 12:04:59 +0530

    Update facultyRoutes.js

--- routes/facultyRoutes.js
+++ routes/facultyRoutes.js
@@ -1,10 +1,11 @@
+// facultyRoutes.js
 const express = require('express');
 const { createFaculty, getFaculty, updateFaculty, deleteFaculty } = require('../controllers/facultyController');
 const router = express.Router();
 
-router.post('/', createFaculty);           // POST /api/faculty
-router.get('/', getFaculty);               // GET /api/faculty
-router.put('/:id', updateFaculty);         // PUT /api/faculty/:id
-router.delete('/:id', deleteFaculty);      // DELETE /api/faculty/:id
+router.post('/', createFaculty);
+router.get('/', getFaculty);
+router.put('/:id', updateFaculty);
+router.delete('/:id', deleteFaculty);
 
 module.exports = router;
